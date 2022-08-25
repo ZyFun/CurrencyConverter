0b704db4e3814c79d6baa9581cd586dd0782bb0f
Dmitriy Danilin <danilindn@icloud.com>
2022-08-25 20:42:58 +0300

    feat: Добавил конвертацию валют
    edit:  Изменил названия полей

--- CurrencyConverter/Presentation/Сalculator/CalculatorViewController.swift
+++ CurrencyConverter/Presentation/Сalculator/CalculatorViewController.swift
@@ -23,8 +23,8 @@ final class CalculatorViewController: UIViewController {
     @IBOutlet weak var firstCharCodeLabel: UILabel!
     @IBOutlet weak var secondCharCodeLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var firstSumTF: UITextField!
-    @IBOutlet weak var secondSumTF: UITextField!
+    @IBOutlet weak var amountCurrencyTF: UITextField!
+    @IBOutlet weak var amountRubTF: UITextField!
     
     // MARK: - LifeCycle
     
@@ -36,9 +36,11 @@ final class CalculatorViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func valueFirstTFDidChanged() {
+        convertToRub()
     }
     
     @IBAction func valueSecondTFDidChanged() {
+        convertToCurrency()
     }
 }
 
@@ -52,6 +54,38 @@ private extension CalculatorViewController {
     func setupNavigationBar() {
         navigationItem.largeTitleDisplayMode = .never
         title = currentCurrency?.name
+    }
+    
+    
+    func convertToRub() {
+        guard let nominal = currentCurrency?.nominal?.doubleValue else { return }
+        guard var rubValue = currentCurrency?.valueRub?.doubleValue else { return }
+        guard let amountCurrency = amountCurrencyTF.text?.doubleValue else { return }
+        
+        if nominal != 1 {
+            rubValue /= nominal
+        }
+        
+        let conversionResult = amountCurrency * rubValue
+        
+        amountRubTF.text = String(format: "%.4f", conversionResult)
+        resultLabel.text = String(format: "Сумма: %.4f RUB", conversionResult)
+    }
+    
+    func convertToCurrency() {
+        guard let charCode = currentCurrency?.charCode else { return }
+        guard let nominal = currentCurrency?.nominal?.doubleValue else { return }
+        guard var rubValue = currentCurrency?.valueRub?.doubleValue else { return }
+        guard let amountRub = amountRubTF.text?.doubleValue else { return }
+        
+        if nominal != 1 {
+            rubValue /= nominal
+        }
+        
+        let conversionResult = 1 / rubValue * amountRub
+        
+        amountCurrencyTF.text = String(format: "%.4f", conversionResult)
+        resultLabel.text = String(format: "Сумма: %.4f \(charCode)", conversionResult)
     }
 }
 
